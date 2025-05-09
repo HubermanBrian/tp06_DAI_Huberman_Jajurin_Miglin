814dbbda88625e595041fe7c2be1572e67ca3440
BRIAN GABRIEL HUBERMAN <48179712@ort>
2025-05-09 08:51:14 -0300

    statusBar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Notice that the status bar has light text!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -18,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
